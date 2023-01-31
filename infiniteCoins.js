0226c112feed098cf4137aa16046cae4cf55c0fc
DDanDev <110492441+DDanDev@users.noreply.github.com>
2023-01-31 18:24:57 -0300

    clean up + explanation in js

--- infiniteCoins.js
+++ infiniteCoins.js
@@ -1,77 +1,34 @@
-//debugs enable:
+//debugs (log step by step and sum on table) enable:
+//to remove this from code, delete this block (before next comment) and all lines starting with debugMessages ?
 let debugMessages = false;
 document.getElementById("enableLog").addEventListener("click", (e) => {
 	debugMessages ? (debugMessages = false) : (debugMessages = true);
 	displayResult(makeChange(changeInput.value));
 });
 const logBox = document.getElementById("stepsLog");
-
-//Get input and display result
-const changeInput = document.getElementById("makeChangeInput");
-const resultDisplay = document.getElementById("makeChangeResults");
-changeInput.addEventListener("change", () => {
-	resultDisplay.innerHTML = "Thinking";
-	setTimeout(() => {
-		if (changeInput.value > 500) {
-			displayResult("Too much");
-			return;
-		}
-		displayResult(makeChange(changeInput.value));
-	}, 1);
-});
-
-function displayResult(resultArray) {
-	let resultHTML = "";
-
-	if (typeof resultArray !== "object") {
-		resultDisplay.innerHTML = resultArray;
-		return;
-	}
-
-	for (result of resultArray) {
-		//test: display sum of each combination too
-
-		debugMessages ? result.push(result[0] * 25 + result[1] * 10 + result[2] * 5 + result[3] * 1) : null;
-
-		resultHTML = `</tr>` + resultHTML;
-		let newRow = "";
-		for (coin of result) {
-			newRow += `<td>` + coin + `</td>`;
-		}
-
-		resultHTML = `<tr>` + newRow + resultHTML;
-	}
-	resultHTML =
-		`<tr>
-            <th>Quarters (25)</th>
-            <th>Dimes (10)</th>
-            <th>Nickels (5)</th>
-            <th>Pennies (1)</th>` +
-		(debugMessages ? `<th>Sum</th>` : "") +
-		`</tr>` +
-		resultHTML;
-	resultDisplay.innerHTML = resultHTML;
-}
-
-displayResult(makeChange(changeInput.value));
+let logContents;
 
 //Calculate
+//Starts at quarters, then for each amount of quarters including zero that fits into total change, checks how many dimes fit in the remaining value.
+//Then for each dime amount possible, check how many nickels can fit in remaining.
+//Then for each nickel amount possible, complete the remaining with pennies.
+//When set is finished with the remaining pennies, it is added to the Nickels list. 
+//After checking pennies for each possible amount of nickels, the array of possible sets is returned to the dime instance where the items are spread and added to the list of possible sets in that instance.
+//Finished dimes are added to quarter arrays in the same way.
+//When this whole cycle finishes for all possible quarter amounts, the aggregated list of sets is returned by remainingCoin, spread and then returned by makeChange.
+//makeChange really only sets the starting point for the recursive remainingToCoin which does the whole thing.
 
 function makeChange(totalChange) {
-	let possibleCombinations = [];
-
-	possibleCombinations.push(...remainingToCoin(25, totalChange, [0, 0, 0, 0]));
-	debugMessages ? (logBox.innerText += `--------------------\n--------------\nFINISHED ALL COMBINATIONS\nwith ` + possibleCombinations.length + ` possibilities!\n--------------\n--------------\n`) : null;
-
-	return possibleCombinations;
+	return [...remainingToCoin(25, totalChange, [0, 0, 0, 0])];
 }
 
 function remainingToCoin(coinValue, remaining, currentSet) {
-	let possibleCoinStarts = [];
+	let possibleSets = [];
 	let maxCoins = Math.floor(remaining / coinValue);
+	let iterationSet = [...currentSet];
+
 	let nextCoinValue;
 	let coinPositionInSet;
-
 	switch (coinValue) {
 		case 1:
 			coinPositionInSet = 3;
@@ -92,21 +49,69 @@ function remainingToCoin(coinValue, remaining, currentSet) {
 	}
 
 	if (nextCoinValue === 0) {
-		let iterationSet = [...currentSet];
 		iterationSet[3] = remaining;
-
-		debugMessages ? (logBox.innerText += "finished one (going back up from pennies) > " + iterationSet + "\n") : null;
+		debugMessages ? (logContents += "finished one, save it (going back up from pennies) > " + iterationSet + "\n") : null;
 		return [iterationSet];
 	} else {
 		for (let currentCoins = 0; currentCoins <= maxCoins; currentCoins++) {
-			let remainingChange = remaining - coinValue * currentCoins;
-			let iterationSet = [...currentSet];
 			iterationSet[coinPositionInSet] = currentCoins;
+			let remainingChange = remaining - coinValue * currentCoins;
 
-			debugMessages ? (logBox.innerText += "checking for " + currentCoins + " coins of value " + coinValue + "(max fit: " + maxCoins + "). Current set: " + iterationSet + "\n") : null;
-			possibleCoinStarts.push(...remainingToCoin(nextCoinValue, remainingChange, iterationSet));
+			debugMessages ? (logContents += "checking for " + currentCoins + " coins of value " + coinValue + "(max fit: " + maxCoins + "). Current set: " + iterationSet + "\n") : null;
+			possibleSets.push(...remainingToCoin(nextCoinValue, remainingChange, iterationSet));
 		}
-		debugMessages ? (logBox.innerText += "going back up from " + coinValue + "\n") : null;
-		return possibleCoinStarts;
+		debugMessages ? (logContents += "going back up from " + coinValue + "\n") : null;
+		return possibleSets;
 	}
 }
+
+/////////////////////////////////////////////////
+//Get input and display result on HTML webpage.//
+/////////////////////////////////////////////////
+const changeInput = document.getElementById("makeChangeInput");
+const resultDisplay = document.getElementById("makeChangeResults");
+changeInput.addEventListener("change", () => {
+	resultDisplay.innerHTML = "Thinking";
+	debugMessages ? (logContents = ``) : null;
+	setTimeout(() => {
+		if (changeInput.value > 500) {
+			displayResult("Too much");
+			return;
+		}
+		displayResult(makeChange(changeInput.value));
+	}, 1);
+});
+
+function displayResult(resultArray) {
+	let resultHTML = "";
+	debugMessages ? (logContents += `--------------------\n--------------\nFINISHED ALL COMBINATIONS\nwith ` + resultArray.length + ` possibilities!\n--------------\n--------------\n`) : (logBox.innerText = ``);
+
+	if (typeof resultArray !== "object") {
+		resultDisplay.innerHTML = resultArray;
+		logContents = "";
+	} else {
+		for (result of resultArray) {
+			debugMessages ? result.push(result[0] * 25 + result[1] * 10 + result[2] * 5 + result[3] * 1) : null;
+
+			resultHTML = `</tr>` + resultHTML;
+			let newRow = "";
+			for (coin of result) {
+				newRow += `<td>` + coin + `</td>`;
+			}
+			resultHTML = `<tr>` + newRow + resultHTML;
+		}
+		resultHTML =
+			`<tr>
+    <th>Quarters (25)</th>
+    <th>Dimes (10)</th>
+    <th>Nickels (5)</th>
+    <th>Pennies (1)</th>` +
+			(debugMessages ? `<th>Sum</th>` : "") +
+			`</tr>` +
+			resultHTML;
+		resultDisplay.innerHTML = resultHTML;
+	}
+	debugMessages ? (logBox.innerText = logContents) : null;
+}
+
+displayResult(makeChange(changeInput.value));
--- readme.md
+++ readme.md
@@ -1,10 +1,6 @@
 Calculate all possible combinations for a given change using quarters, dimes, nickels and pennies (25, 10, 5 and 1 cent values respectively).
 
-https://ddandev.github.io/coin-change-calculator/
+Use it at https://ddandev.github.io/coin-change-calculator/
 
 Optional:
-See it step by step! Click the button to enable log messages for each step calculated and display sum in table to check each result row.
-
-Next:
-Clear log after recalculate
-make log more readable
+See it step by step! Click the button to enable log messages for each step calculated and display sum in table to confirm each result row is correct.
